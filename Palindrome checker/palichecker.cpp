3ae9885042a51691c9fcdcff9bce209dd154deec
Joshua Harvey <jharvey4530@conestogac.on.ca>
2024-03-24 01:57:52 -0400

    added for loop and scanf statement user input done for the most part

--- Palindrome checker/palichecker.cpp
+++ Palindrome checker/palichecker.cpp
@@ -1,8 +1,21 @@
 #include <stdio.h>
 
 
+void get_input() {
+	char palindrome[10];
+	
+
+	for (int i = 0; i < 10; i++) {
+		printf("please enter in the word or Phrase You'd like to check:");
+		scanf_s("%s", &palindrome, sizeof(palindrome));
+	}
+}
+
+
 int main() {
 
-
+	get_input();
 	return 0;
 }
+
+
