275b2e88f501c16dab139022cdb8d230722eccd6
TwentyFifthbam-SWE <joshuaharvey2010@gmail.com>
2024-04-05 20:37:06 -0400

    have 1  error checker for main working on other functions

--- majo4/majo4/m4.cpp
+++ majo4/majo4/m4.cpp
@@ -1,11 +1,42 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
+#include <errno.h>
 
 
-int main(void) {
 
-	return 0;
+
+
+
+
+
+
+void processFlight() {
+	FILE *ftptr;
+	
+
+
+}
+
+
+
+int main() {
+	FILE* ftptr = fopen("C:\\Users\\Redme\\OneDrive\\Documents\\flights.txt","r");
+	char buffer[255];
+	
+	
+
+	errno_t err = fopen_s(&ftptr, "C:\\Users\\Redme\\OneDrive\\Documents\\flights.txt", "r");
+	
+	if (ftptr == NULL) {
+		printf("Error file does not exist please try  again!");
+
+		return 1;
+	}
+   
+	
+
+   
 
 
 
