61455ed8766997c96a89eb0b296d15e77b8ebbd5
TwentyFifthbam-SWE <joshuaharvey2010@gmail.com>
2024-04-05 21:20:42 -0400

    figured out Process flight function on to  displayleastfare function

--- majo4/majo4/m4.cpp
+++ majo4/majo4/m4.cpp
@@ -3,19 +3,49 @@
 #include <ctype.h>
 #include <errno.h>
 
+#define Max_length 100
 
 
 
+
+
+
+
+
+
+
+
+
+
+struct Flight_Info {
+	int Fl_Info_Array[Max_length];
+	
+};
 
 
 
 
 
 void processFlight() {
+	struct Flight_Info flightinfo;
 	FILE *ftptr;
+	char buffer[255];
 	
 
 
+	ftptr = fopen("C:\\Users\\Redme\\OneDrive\\Documents\\flights.txt","r");
+	
+	if (ftptr == NULL) {
+		printf("Error Opening file");
+		return;
+		}
+
+	fgets(buffer, sizeof(buffer), ftptr);
+	for(int i = 0; i < Max_length; i++){
+		flightinfo.Fl_Info_Array[i] = i * 2;
+	}
+
+	fclose(ftptr);
 }
 
 
