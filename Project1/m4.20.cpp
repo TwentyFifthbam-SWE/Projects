34d448557de5b9b185fa1f5f413b4921a1d30fdc
Joshua Harvey <joshuaharvey2010@gmail.com>
2024-04-06 15:25:36 -0400

    I believe everything is done but still have to do of course go through 1 error 16 warnings  most of it to do with line 93 and line 11 also theres 115, 9,10 which are just warnings one their own

--- Project1/m4.20.cpp
+++ Project1/m4.20.cpp
@@ -36,8 +36,34 @@ void displayLeastFareDetails(struct FlightEntry flights[], int numflights) {
     FILE* WESTJET;
     FILE* Aircanada;
     struct FlightEntry leastfare[MAX_AIRLINES];
-    for (int i = 0; i < MAX_AIRLINES; i++); {
+    
+    
+    
+    for (int i = 0; i < MAX_AIRLINES; i++) {
+        leastfare[i].Fare = INT_MAX;
+   }
+    for (int i = 0; i < numflights; i++) {
+        struct FlightEntry currentFlight = flights[i];
+        int j = 0;
+        while(j < MAX_FLIGHTS){
+            if (strcmp(leastfare[j].source, currentFlight.source) == 0 &&
+                strcmp(leastfare[i].Destination, currentFlight.source) == 0)
+            {
+                 if (currentFlight.Fare < leastfare[j].Fare) {
+                    leastfare[j] = currentFlight;
+               }
+                break;
+            }
+            j++;
+    
+    }
 
+    }
+    for (int i = 0; i < MAX_FLIGHTS; i++)
+    {
+        if (leastfare[i].Fare != INT_MAX) {
+            printf("%s: %s to %s, Fare $%d\n", leastfare[i].airline, leastfare[i].source, leastfare[i].Destination, leastfare[i].Fare);
+        }
     }
 
     
@@ -74,28 +100,35 @@ int parseline(char* source,char* destination,int* fare,char* read) {
     }
 }
 
+void processFlight(const char* filename,struct FlightEntry flights[], int* totalCount) {
+  
+    FILE* source;
 
-
+    errno_t err = fopen_s(&source, filename, "r");
+    if (err != 0 || source == NULL) {
+        printf("error opening file %s.\n", filename);
+        return;
+    }
+    char line[100];
+    while (fgets(line,sizeof(line),source) != NULL && *totalCount <MAX_FLIGHTS)
+    {
+        sscanf(line, "%[^,],%[^,],%[^,],%d", flights[*totalCount].airline,
+            flights[*totalCount].source, flights[*totalCount].Destination,
+            &flights[*totalCount].Fare);
+        (*totalCount)++;
+    }
+    fclose(source);
+}
 
 
 
 int main() {
-   
+  
+    struct FlightEntry flights[MAX_FLIGHTS];
     
-    char buffer[255];
-    char* flights_txt;
-    const char* source = "C:\\Users\Josh\Downloads\teams.txt";
-    flights_txt = (char*)malloc(100 * sizeof(char));
+    int totalCount = 0;
    
-    if (strcpy_s(flights_txt, sizeof(flights_txt), source) != 0) {
-        printf("Copying failed\n");
-        return 1;
-    }
-   
-    char* fgets(char* flights_txt,  char* source);
-    
-    
-    return 0;
-
+    processFlight("Air Canada.txt", flights, &totalCount);
+    processFlight("Westjest.txt", flights, &totalCount);
 
 }
